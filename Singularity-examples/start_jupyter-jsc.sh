0a390950853db17ceee4f511d82a4f3de84a62c0
Katharina Höflich <37177103+kathoef@users.noreply.github.com>
2021-01-19 18:53:37 +0100

    Provide absolute config path

--- Singularity-examples/start_jupyter-jsc.sh
+++ Singularity-examples/start_jupyter-jsc.sh
@@ -4,4 +4,4 @@
 # Repository: https://github.com/FZJ-JSC/jupyter-jsc-notebooks
 
 SINGULARITY_IMAGE=/p/project/cesmtst/hoeflich1/jupyter-base-notebook/jupyter-base-notebook.sif
-JUPYTERJSC_USER_CMD="singularity exec ${SINGULARITY_IMAGE} jupyterhub-singleuser --config ${OLDPWD}/.config.py"
+JUPYTERJSC_USER_CMD="singularity exec ${SINGULARITY_IMAGE} jupyterhub-singleuser --config ${JUPYTER_LOG_DIR}/.config.py"
